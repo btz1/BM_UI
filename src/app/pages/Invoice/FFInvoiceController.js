749a6f660e80ccad6c7ce8163eabd2fa7cd99c8e
Abubakar <abubakar910@gmail.com>
2018-03-02 23:13:50 +0500

    invoice printing

--- src/app/pages/Invoice/FFInvoiceController.js
+++ src/app/pages/Invoice/FFInvoiceController.js
@@ -9,9 +9,8 @@
         .controller('FFInvoiceController', FFInvoiceController);
 
     /** @ngInject */
-    function FFInvoiceController ($scope,$http)
+    function FFInvoiceController ($scope,$http,$timeout)
     {
-
         $scope.invoice = {
             items: [{
                 name: 'item',
@@ -33,17 +32,16 @@
                 price: 0
             });
         },
-            $scope.remove = function(index){
-                $scope.invoice.items.splice(index, 1);
-            },
-            $scope.total = function(){
-                var total = 0;
-                angular.forEach($scope.invoice.items, function(item){
-                    total += item.qty * item.price;
-                })
-                return total;
-            }
-
+        $scope.remove = function(index){
+            $scope.invoice.items.splice(index, 1);
+        },
+        $scope.total = function(){
+            var total = 0;
+            angular.forEach($scope.invoice.items, function(item){
+                total += item.qty * item.price;
+            })
+            return total;
+        };
 
         $scope.customer ="";
         $scope.product="";
@@ -54,9 +52,20 @@
 
         $scope.printToCart = function(printSectionId) {
             var innerContents = document.getElementById(printSectionId).innerHTML;
-            var popupWinindow = window.open('', '_blank', 'width=600,height=700,scrollbars=no,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
+            var popupWinindow = window.open('', '_blank', 'width=4000,height=4000,scrollbars=no,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
             popupWinindow.document.open();
-            popupWinindow.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + innerContents + '</html>');
+            popupWinindow.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" />' +
+                '</head><body onload="window.print()"> <section class="row" ng-show="false" > <div style="width: 100%;"> ' +
+                '<img src="../../../assets/img/header.jpg" width="100%" height="250px" /><br/><br/><br/> ' +
+                '<table class="table table-hover" style="border:1px solid black;width: 100%"> ' +
+                '<thead> <tr> ' +
+                '<th style="border:1px solid black;padding: 10px;">Name</th> ' +
+                '<th style="border:1px solid black;padding: 10px;">Description</th> ' +
+                '<th style="border:1px solid black;padding: 10px;">Qty</th> ' +
+                '<th style="border:1px solid black;padding: 10px;">Price</th> ' +
+                '<th style="border:1px solid black;padding: 10px;">Total</th> ' +
+                '</tr> </thead>' + innerContents + ' </table> ' +
+                '<br/><br/> <img src="../../../assets/img/footer.jpg" width="100%" height="100px" /> </div> </section> </html>');
             popupWinindow.document.close();
         };
 
