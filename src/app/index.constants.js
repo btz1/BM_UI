37a3606b383fe093f353425fd8dc4bc587d3d65d
zeeshanramzan <zeeshan.ramzan01@gmail.com>
2018-03-25 02:15:58 +0500

    Customer Page Updated

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -1,9 +1,9 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('BlurAdmin')
 
-        .constant('apiUrl', "http://localhost:8080/");
+        .constant('apiUrl', "http://192.168.8.104:8080/");
 
 })();
